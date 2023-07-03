2b847f10b315d9213188e014fc44f524b116b8d7
AJY <135806539+nobiajay@users.noreply.github.com>
2023-07-02 23:14:33 -0700

    inherit some audio files

--- common.mk
+++ common.mk
@@ -1,1 +1,13 @@
+#
+# Copyright (C) 2021-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+PRODUCT_COPY_FILES += \
+$(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_holi/audio_io_policy.conf)   
+
+
+PRODUCT_COPY_FILES += \
+$(LOCAL_PATH)/audio/bluetooth_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_hearing_aid_audio_policy_configuration.xml
+$(LOCAL_PATH)/audio/audio_policy_configuration_a2dp_offload_disabled.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_a2dp_offload_disabled.xml \
 
